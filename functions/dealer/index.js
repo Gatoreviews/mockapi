d663f84f172c3bad9c4bae181dc5d156cd5799ea
GGODDET <glmg73@gmail.com>
2021-07-09 16:29:52 +0200

    update dealer to POST

--- functions/dealer/index.js
+++ functions/dealer/index.js
@@ -1,3 +1,5 @@
+const _ = require("lodash")
+
 exports.handler = async event => {
   const e = [
     {
@@ -5,66 +7,81 @@ exports.handler = async event => {
       status: 0,
       revert_link: "",
       lead_accepted: "",
+      body: { lead_accepted: "1" },       
     },
     {
       token: "DEALER_ACCEPTED",
       status: 1,
       revert_link: "DEALER_NEW_REJECTED?lead_accepted=0",
       lead_accepted: 1,
+      body: { lead_accepted: "0" },      
     },
     {
       token: "DEALER_REJECTED",
       status: 1,
       revert_link: "DEALER_NEW_ACCEPTED?lead_accepted=1",
       lead_accepted: 0,
+      body: { lead_accepted: "1" },      
     },
     {
       token: "DEALER_NEW_REJECTED",
       status: 1,
       revert_link: "",
       lead_accepted: 0,
+      body: { lead_accepted: "1" },      
     },
     {
       token: "DEALER_NEW_ACCEPTED",
       status: 1,
       revert_link: "",
       lead_accepted: 1,
+      body: { lead_accepted: "0" },      
     },
     {
       token: "DEALER_ACCEPTED_NEW_EXPIRED",
       status: 1,
       revert_link: "DEALER_NEW_REJECTED_EXPIRED?lead_accepted=0",
       lead_accepted: 1,
+      body: { lead_accepted: "0" },      
     },
     {
       token: "DEALER_REJECTED_NEW_EXPIRED",
       status: 1,
       revert_link: "DEALER_NEW_ACCEPTED_EXPIRED?lead_accepted=1",
       lead_accepted: 0,
+      body: { lead_accepted: "1" },      
     },
     {
       token: "DEALER_NEW_REJECTED_EXPIRED",
       status: 0,
       revert_link: "",
       lead_accepted: "",
+      body: { lead_accepted: "1" },      
     },
     {
       token: "DEALER_NEW_ACCEPTED_EXPIRED",
       status: 0,
       revert_link: "",
       lead_accepted: "",
+      body: { lead_accepted: "0" },      
     },
   ]
 
   const token = event.queryStringParameters.token
+  const body = JSON.parse(event.body ? event.body : "{}")
   return {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Methods": "GET, POST",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(e.find(el => el.token == token && el.body ? el.body == JSON.parse(event.body) : true) || {}),
+    body: JSON.stringify(
+      _.omit(
+        e.find(el => el.token == token && _.isEqual(body, el.body)),
+        ["body"]
+      ) || {}
+    ),
   }
 }
